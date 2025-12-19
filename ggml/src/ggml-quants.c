653a9feb3aae840610b9afc96f7158cc32a33e2c
Geoff Munn <geoff.munn@mrapple.com>
2025-12-19 21:11:00 +1300

    Syntax errors fixed

--- ggml/src/ggml-quants.c
+++ ggml/src/ggml-quants.c
@@ -5465,6 +5465,12 @@ bool ggml_validate_row_data(enum ggml_type type, const void * data, size_t nbyte
                 VALIDATE_ROW_DATA_D_F16_IMPL(block_q3_hifi, data, nb);
             } break;
 
+        case GGML_TYPE_Q3_HIFI_SCALE:
+            {
+                // Q3_HIFI_SCALE uses same block format as Q3_K
+                VALIDATE_ROW_DATA_D_F16_IMPL(block_q3_K, data, nb);
+            } break;
+
         case GGML_TYPE_I8:
         case GGML_TYPE_I16:
         case GGML_TYPE_I32:
